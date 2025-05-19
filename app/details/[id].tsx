f61fc069f28e8aca5a8ee521da9508c6e81586d0
Kelvin <74870436+JRSKelvin@users.noreply.github.com>
2025-05-19 18:58:19 +0700

    add: add chart for detail page with minor styling and content

--- app/details/[id].tsx
+++ app/details/[id].tsx
@@ -1,14 +1,16 @@
+import { ThemedText } from '@/components/ThemedText';
 import { ThemedView } from '@/components/ThemedView';
 import { useGetBySymbolCryptoQuery } from '@/services';
 import dayjs from 'dayjs';
 import { useLocalSearchParams } from 'expo-router';
 import React from 'react';
-import { Dimensions } from 'react-native';
+import { Dimensions, StyleSheet } from 'react-native';
 import { LineChart } from 'react-native-chart-kit';
 
 export default function DetailScreen() {
   const { id } = useLocalSearchParams();
   const [chartData, setChartData] = React.useState<any>({ labels: [], datasets: [{ data: [] }] });
+  const [infoData, setInfoData] = React.useState<any>({ currentPrice: 0 });
   const { data: cryptoListData } = useGetBySymbolCryptoQuery(id as string);
   React.useEffect(() => {
     const labels: any[] = [];
@@ -20,24 +22,53 @@ export default function DetailScreen() {
       labels.push(dayjs(timestamp).format("HH:mm"));
       firstData.data.push(data["1. open"]);
     });
+    setInfoData((current: any) => ({ ...current, currentPrice: firstData.data[0] }));
     firstData.data = firstData.data.reverse();
     datasets.push(firstData);
     setChartData({ labels: labels.reverse(), datasets: datasets });
   }, [cryptoListData]);
   return (
     <ThemedView>
-      {/* <ThemedText>{id}</ThemedText> */}
-      <LineChart
-        data={{
-          labels: chartData?.labels || [],
-          datasets: chartData?.datasets || [],
-        }}
-        height={Dimensions.get("window").height / 3}
-        width={Dimensions.get("window").width}
-        chartConfig={{
-          color: (opacity = 1) => `rgba(0, 123, 255, ${opacity})`,
-        }}
-      />
+      <ThemedView>
+        <ThemedText style={styles.titleSymbol}>{id}</ThemedText>
+        <ThemedText style={styles.subtitleCurrentPrice}>$ {infoData?.currentPrice}</ThemedText>
+        <LineChart
+          style={styles.chart}
+          data={{
+            labels: chartData?.labels || [],
+            datasets: chartData?.datasets || [],
+          }}
+          height={Dimensions.get("window").height / 3}
+          width={Dimensions.get("window").width}
+          chartConfig={{
+            backgroundColor: 'transparent',
+            backgroundGradientFrom: 'transparent',
+            backgroundGradientTo: 'transparent',
+            color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+          }}
+        />
+      </ThemedView>
     </ThemedView>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+  },
+  titleSymbol: {
+    padding: 24,
+    textAlign: 'center',
+    fontSize: 24,
+  },
+  subtitleCurrentPrice: {
+    padding: 24,
+    textAlign: 'center',
+    fontSize: 40,
+  },
+  chart: {
+    paddingTop: 24,
+    paddingBottom: 24,
+  }
+});
